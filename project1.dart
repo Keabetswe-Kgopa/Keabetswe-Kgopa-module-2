73f37f14a8d08107b9f42ae56337cbc0a0394acd
Keabetswe-Kgopa <105922342+Keabetswe-Kgopa@users.noreply.github.com>
2022-05-20 14:00:37 +0200

    Update project1.dart

--- project1.dart
+++ project1.dart
@@ -1,7 +1,7 @@
 void main() {
-  var name = Kgopa Keabetswe;
-  var favourate_app = MTN App;
-  var city = Vanderbijlpark;
+  var name = "Kgopa Keabetswe";
+  var favourate_app = "MTN App";
+  var city = "Vanderbijlpark";
   
   print("$name");
   print("$favourate_app");
